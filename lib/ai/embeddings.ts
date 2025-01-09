c9aa1b2d4f424a546fa1156012b319a6fa4f968f
Gabriel Fritz <gabrielfritzsluzala@gmail.com>
2025-01-08 21:48:56 -0300

    Bug fix

--- lib/ai/embeddings.ts
+++ lib/ai/embeddings.ts
@@ -1,7 +1,7 @@
 import { openai } from '@ai-sdk/openai';
-import { embed, UserContent } from 'ai';
+import { embed, UserContent, EmbeddingModel } from 'ai';
 
-const embeddingModel = openai('text-embedding-3-small');
+const embeddingModel = openai.embedding('text-embedding-3-small');
 
 export const generateEmbeddings = async (content: UserContent) => {
     const { embedding } = await embed({
