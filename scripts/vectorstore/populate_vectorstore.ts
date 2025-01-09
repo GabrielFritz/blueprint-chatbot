6dcbf1f37df3154a13247a92c817cb87b8b238e8
Gabriel Fritz <gabrielfritzsluzala@gmail.com>
2025-01-08 23:48:00 -0300

    Testing something

---
+++ scripts/vectorstore/populate_vectorstore.ts
@@ -0,0 +1,27 @@
+import { embeddings } from '@/lib/db/schema';
+import { data } from '@/scripts/vectorstore/data';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+if (!process.env.POSTGRES_URL) {
+    throw new Error('POSTGRES_URL is not defined');
+  }
+
+  const connection = postgres(process.env.POSTGRES_URL);
+  const db = drizzle(connection);
+
+const populateVectorStore = async () => {
+
+    await db.delete(embeddings);
+    for (const [index, item] of data.entries()) {
+        console.log(`Processing item ${index + 1} of ${data.length} - ${item.id}`);
+        await db.insert(embeddings).values({
+            id: item.id,
+            content: item.content,
+            embedding: item.embedding,
+        });
+    }
+    console.log('✅ Vector store populated');
+};
+
+populateVectorStore();
